e417de033d03293cfe77dd815b7d19e82a1192ea
e0323290 <ayh_janel@hotmail.com>
2019-08-22 16:06:41 +0800

    level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,14 +9,26 @@ public class Duke {
      */
 
     public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Duke \nWhat can I do for you?");
         String inputString = scanner.nextLine();
+
+        int count = 0;
         while(!inputString.equals("bye")) {
-            System.out.println(inputString);
-            inputString = scanner.nextLine();
+            if(inputString.equals("list")) {
+                for(int i = 0; i < arrayList.size(); i++) {
+                    System.out.print(arrayList.get(i) + "\n");
+                }
+                inputString = scanner.nextLine();
+            }
+            else {
+                count++;
+                System.out.println("added: " + inputString);
+                arrayList.add(count + ". " + inputString);
+                inputString = scanner.nextLine();
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
-
 }

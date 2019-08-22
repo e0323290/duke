80cb8b19f0e40cfed21729c9c242f78cede10b4f
e0323290 <ayh_janel@hotmail.com>
2019-08-22 15:25:49 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,22 @@
+import java.util.*;
+import java.lang.*;
+
 public class Duke {
+    /**
+     * Main class.
+     *
+     * @param args empty
+     */
+
     public static void main(String[] args) {
-        System.out.println("Hello from\n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke \nWhat can I do for you?");
+        String inputString = scanner.nextLine();
+        while(!inputString.equals("bye")) {
+            System.out.println(inputString);
+            inputString = scanner.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
+
 }
